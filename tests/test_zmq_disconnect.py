6cd8306a3ad222c27911d8de768deb7e1439cf57
Kafonek <matt.kafonek@noteable.io>
2023-02-24 09:28:27 -0500

    tweak test, was behaving differently on local and CI

--- tests/test_zmq_disconnect.py
+++ tests/test_zmq_disconnect.py
@@ -1,5 +1,4 @@
 import collections
-import textwrap
 from typing import Optional
 
 from jupyter_client import KernelConnectionInfo
@@ -19,28 +18,27 @@ class DisconnectHandlingClient(SidecarKernelClient):
     async def handle_zmq_disconnect(self, channel_name: str):
         # We might miss messages like execute_reply or status while reconnecting, which would cause
         # the currently-running action to never resolve it's "done" event (await action).
-        # Setting the two event states here will make the "await action" resolve when either status
-        # or reply event is seen next.
-        # Not a perfect solution, tune this behavior in your own app.
-        self.running_action.kernel_idle.set()
-        self.running_action.reply_seen.set()
+        # For this test class, "resolve" the awaitable as soon as a disconnect happens.
+        # You might want to do something different in your own prod app
+        self.running_action.done.set()
         self.channel_disconnects[channel_name] += 1
 
 
 async def test_zmq_disconnect(ipykernel: dict):
     """
-    Show that
+    Show that a subclassed Kernel with zmq handling hooks takes action when zmq iopub channel
+    gets disconnected because a message is larger than the max_message_size setting.
     """
     async with DisconnectHandlingClient(connection_info=ipykernel, max_message_size=1024) as kernel:
+        # A small stream output should come through fine
         handler = DebugHandler()
-        code = textwrap.dedent(
-            """
-        print('x' * 2048)
-        """
-        )
-        action = kernel.execute_request(code=code, handlers=[handler])
+        action = kernel.execute_request(code="print('x')", handlers=[handler])
         await action
-        # TODO: watch for this test getting flaky? On local dev, it always misses the second status
-        # message during reconnect
-        assert handler.counts == {"status": 1, "execute_input": 1, "execute_reply": 1}
+        assert handler.counts == {"status": 2, "execute_input": 1, "execute_reply": 1, "stream": 1}
+
+        # A stream size larger than max_message_size should cause a disconnect
+        handler = DebugHandler()
+        action = kernel.execute_request(code=" print('x' * 2048)", handlers=[handler])
+        await action
+        assert handler.counts == {"status": 1, "execute_input": 1}
         assert kernel.channel_disconnects == {"iopub": 1}
