f5c02c45699d0b6518726d6b785e301d30072124
Kafonek <matt.kafonek@noteable.io>
2023-04-14 09:16:08 -0400

    add env prefix

--- src/kernel_sidecar/settings.py
+++ src/kernel_sidecar/settings.py
@@ -6,6 +6,9 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     pprint_logs: bool = False
 
+    class Config:
+        env_prefix = "kernel_sidecar_"
+
 
 # cached get_settings
 @functools.lru_cache()
